f73b8bce0305672ea1cf8ef95355e6e4b0f1f5b3
usamallgr <150150540+usamallgr@users.noreply.github.com>
2023-11-08 17:19:43 +0100

    Add files via upload

---
+++ jvjs.js
@@ -0,0 +1,10 @@
+        // Afficher le cercle de chargement
+        var loadingCircle = document.getElementById("loadingCircle");
+        loadingCircle.style.display = "block";
+
+        // Attendre 2 secondes (2000 millisecondes) avant d'afficher le contenu de la page
+        setTimeout(function() {
+            loadingCircle.style.display = "none";
+            var pageContent = document.getElementById("pageContent");
+            pageContent.style.display = "block";
+        }, 999); // 2000 ms = 2 secondes
